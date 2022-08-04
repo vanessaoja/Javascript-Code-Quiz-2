cced3f022aa3acc723b6adea4708f12a864397af
vanessaoja <ojavanessa@gmail.com>
2022-08-04 13:34:43 -0500

    added question element

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-const startButton = document.getElementById('start')
-const questionContainerElement = document.getElementById('question-container')
+const startButton = document.getElementById('start');
+const questionContainerElement = document.getElementById('question-container');
+const questionElemenmt = document.getElementById('question');
+const answerButtonElement = document.getElementById('answer-buttons')
+startButton.addEventListener('click', startGame);
 
-startButton.addEventListener('click', startGame)
 
-function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
+function startGame() {;
+    console.log('Started');
+    startButton.classList.add('hide');
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
     setNextQuestion()
-}
+};
 
 function setNextQuestion() {
+};
+
+function showQuestion(question) {
 
 }
 
 function selectAnswer() {
 
-}
+};
 
-const
+const questions = [ 
+{
+    question: 'What language is JavaScript?', 
+    answer: [
+        {text: 'object-oriented', correct: true},
+        {text: 'object-based', correct: false},
+        {text: 'Proceduaral', correct: false},
+        {text: 'Noce of the above', correct: false},
+    ]
+
+}
+]
 
